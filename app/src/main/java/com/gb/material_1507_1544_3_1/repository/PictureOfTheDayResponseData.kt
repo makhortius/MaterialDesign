13027bea2bc7f42d0e193f3fe3c91f26e6fabeaa
Andej Nesterenko <mentatus.n@gmail.com>
2021-12-13 21:37:08 +0300

    урок 1 реализовали работу с сервервером NASA через retrofit2

---
+++ app/src/main/java/com/gb/material_1507_1544_3_1/repository/PictureOfTheDayResponseData.kt
@@ -0,0 +1,14 @@
+package com.gb.material_1507_1544_3_1.repository
+
+import com.google.gson.annotations.SerializedName
+
+data class PictureOfTheDayResponseData(
+    @field:SerializedName("copyright") val copyright: String?,
+    @field:SerializedName("date") val date: String?,
+    @field:SerializedName("explanation") val explanation: String?,
+    @field:SerializedName("media_type") val mediaType: String?,
+    @field:SerializedName("title") val title: String?,
+    @field:SerializedName("url") val url: String?,
+    @field:SerializedName("hdurl") val hdurl: String?
+
+)
