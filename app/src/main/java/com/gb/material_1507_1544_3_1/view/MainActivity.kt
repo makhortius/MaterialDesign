575d20a171b3d299db1ca7d6e81a80a7657ff7fa
Andej Nesterenko <mentatus.n@gmail.com>
2021-12-16 23:49:23 +0300

    урок 2 починили TextAppearance.MaterialComponents.Button (нашли решение здесь https://github.com/material-components/material-components-android/issues/1013)

--- app/src/main/java/com/gb/material_1507_1544_3_1/view/MainActivity.kt
+++ app/src/main/java/com/gb/material_1507_1544_3_1/view/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.gb.material_1507_1544_3_1.view
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatDelegate
 import com.gb.material_1507_1544_3_1.view.chips.SettingsFragment
 import com.gb.material_1507_1544_3_1.view.picture.PictureOfTheDayFragment
 import com.gb.material_1507_1555_3_1.R
@@ -10,15 +13,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        // TODO FULL SCREEN
+        //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+       //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         // если во время исполнения то не забываем выполнить recreate()
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, PictureOfTheDayFragment.newInstance()).commit()
-
             /*supportFragmentManager.beginTransaction()
-                .replace(R.id.container, SettingsFragment.newInstance()).commit()*/
+                .replace(R.id.container, PictureOfTheDayFragment.newInstance()).commit()*/
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, SettingsFragment.newInstance()).commit()
         }
         //recreate()
     }
